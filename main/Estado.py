0cc22c1549395a62d6465f6b4728e33ab6273340
Pablo Porrero Almansa <114909246+ppa21102@users.noreply.github.com>
2022-11-06 13:26:58 +0100

    Update Estado.py

--- main/Estado.py
+++ main/Estado.py
@@ -3,24 +3,24 @@ import hashlib
 
 
 class Estado:
-    
-    def md5generador(self,id_nodo,lista):
-        n=0
-        m = hashlib.md5()
-        m.update(id_nodo.encode())
-        for i in lista:
-            m.update(list[n].encode())
-            n=n+1
-        return m.hexdigest()
 
-    def __init__(self,nodo,lista_nodos,coste):
+    def __init__(self,nodo,lista_nodos, coste):
         self.nodo = nodo
-        self.lista_nodos = sorted(lista_nodos)
-        self.id = self.md5generador(self.nodo,self.lista_nodos)
+        self.listNodos = sorted(lista_nodos)
+        self.id = self.md5generador(self.nodo,self.listNodos)
+        self.coste = coste
+        
+    def md5generador(self,id_nodo,lista):
+        # creamos el estado (nodo actual +  nodos por visitar) y creamos su id en formato md5
+        estado = f'{id_nodo} {lista}'
+        md5 = hashlib.md5(estado.encode())
+        return md5.hexdigest()
 
-    def get_nodo(self):
+    def getNodo(self):
         return self.nodo
 
-    def get_lista_nodos(self):
-        return self.lista_nodos
-
+    def getListaNodos(self):
+        return self.listNodos
+    
+    def getCoste(self):
+        return self.coste
