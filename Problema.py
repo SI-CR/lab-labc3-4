d89ebffc9bdcd50e12c6ad0f0cab22ea83218551
Pablo Porrero Almansa <114909246+ppa21102@users.noreply.github.com>
2022-11-06 13:38:08 +0100

    Create Problema.py

---
+++ Problema.py
@@ -0,0 +1,36 @@
+from EspacioDeEstados import EspacioDeEstados
+from Estado import Estado
+
+class Problema:
+
+    def __init__(self):
+        self.__espacioEstados=EspacioDeEstados("Capitales y Talavera/CR.graphXML")
+
+        #Definicion del EstadoInicial
+        nodoInicial = "3123"
+        listaNodosInicial = ['3161', '3114', '2479', '3365', '3363']
+        
+        existenNodos = True
+        if not self.__espacioEstados.nodoPerteneceGrafo(nodoInicial, listaNodosInicial):
+            existenNodos = False
+                    
+        if (existenNodos):
+            heuristica = 0
+            self.__estadoInicial=Estado(nodoInicial,listaNodosInicial,heuristica)
+        
+            solucion = self.__espacioEstados.sucesores(self.__estadoInicial)
+            for s in solucion:
+                print(s)
+
+    def getEstadoInicial(self):
+        return self.__estadoInicial
+
+    def getEspacioEstados(self):
+        return self.__espacioEstados
+    
+    def esObjetivo(self,estado):
+        return estado.getListaNodos() == []
+    
+if (__name__ == "__main__"):
+    
+    p = Problema()
