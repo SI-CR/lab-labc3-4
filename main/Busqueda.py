49ec5ab21b759dd21026713f7a72c8f23f58243e
Pablo Porrero Almansa <114909246+ppa21102@users.noreply.github.com>
2022-11-27 23:11:53 +0100

    Cambios a la hora de mostrar la solución

--- main/Busqueda.py
+++ main/Busqueda.py
@@ -1,60 +1,79 @@
 from NodoArbol import NodoArbol
 from Frontera import Frontera
-from EspacioDeEstados import EspacioDeEstados
 from Problema import Problema
-from Grafo import Grafo
-import stack
-
-#nodosCreados = 0
 
 
-def __init__(self):
-    pass
+def busquedaSolucion(problema, estrategia, profundidadMaxima):
+
+    solucion = None
+
+    while (solucion == None):
+        solucion, ultimoNodoVisitado = algoritmoBusqueda(
+            problema, estrategia, profundidadMaxima)
+
+    return solucion, ultimoNodoVisitado
 
 
-def generarNodoArbol(padre, estado, coste, estrategia, accion):
-    nodo = NodoArbol(padre, estado, coste, estrategia, accion)
-    return nodo
+def algoritmoBusqueda(problema, estrategia, profundidaMaxima):
+
+    datosPoda = {}
+
+    frontera = Frontera()
+    estadoInicial = problema.getEstadoInicial()
+    nodoInicial = NodoArbol(
+        None, estadoInicial, 0, estrategia, '0.0 0 0.0')
+    frontera.anadirNodo(nodoInicial)
+    solucion = None
+
+    while ((solucion == None) and (not (frontera.estaVacia()))):
+        nodoActual = frontera.sacar()
+        estadoNodoActual = nodoActual.getEstado()
+        if problema.esObjetivo(estadoNodoActual):
+            solucion = True
+        else:
+            sucesoresEstado = problema.getEspacioEstados().sucesores(estadoNodoActual)
+            if sucesoresEstado != None:
+                listaNodos = expandirNodos(
+                    sucesoresEstado, nodoActual, profundidaMaxima, estrategia)
+                nodosPoda, datosPoda = algoritmoPoda(
+                    datosPoda, listaNodos, estrategia)
+                frontera.anadirListaNodos(nodosPoda)
+
+    if (solucion == None):
+        return None, None
+    else:
+        return formatoSolucion(nodoActual), nodoActual
 
 
 def expandirNodos(listaSucesores, padre, profundidadMaxima, estrategia):
+
     listaNodos = []
-    global nodosCreados
-    # print("padre", padre.getProfundidad())
+
     if (padre.getProfundidad() < int(profundidadMaxima)):
         for datosSucesor in listaSucesores:
-            #print("Sucesor", datosSucesor[0], datosSucesor[1].getNodo(
-            #), datosSucesor[1].getListaNodos(), datosSucesor[2])
             coste = datosSucesor[2]
             accion = datosSucesor[0]
             estado = datosSucesor[1]
-            nodo = generarNodoArbol(padre, estado, coste, estrategia, accion)
-            #print(nodo.getAccion(), "COSTE ACUMULADO:", nodo.getCoste(),
-            #      nodo.getEstado().getListaNodos())
+            nodo = NodoArbol(padre, estado, coste, estrategia, accion)
             listaNodos.append(nodo)
 
-            #nodosCreados += 1
     return listaNodos
 
 
 def algoritmoPoda(dictPoda, listaNodos, estrategia):
 
-    ListaNodosPoda=[]
+    ListaNodosPoda = []
 
     for nodo in listaNodos:
-        estadoNodo=nodo.getEstado()
-        idEstadoNodo=estadoNodo.getID()
-        valorNodo=nodo.getValor()
+        idEstadoNodo = nodo.getEstado().getID()
+        valorNodo = nodo.getValor()
 
         if (idEstadoNodo in dictPoda):
             if (estrategia in ["profundidad"]):
-                # print("profundidad")
                 if valorNodo > int(dictPoda.get(idEstadoNodo)):
-                    #print("p")
                     dictPoda.update({idEstadoNodo: valorNodo})
                     ListaNodosPoda.append(nodo)
             else:
-                # print("anchura")
                 if valorNodo < int(dictPoda.get(idEstadoNodo)):
 
                     dictPoda.update({idEstadoNodo: valorNodo})
@@ -67,68 +86,14 @@ def algoritmoPoda(dictPoda, listaNodos, estrategia):
 
 
 def formatoSolucion(nodo):
-    pila=stack.Stack()
 
-    nodoPila=nodo
+    solucion = []
 
-    while (not nodoPila.getPadre() == None):
-        pila.push(nodoPila)
-        nodoPila=nodoPila.getPadre()
-
-    pila.push(nodoPila)
-
-    pilaSolucion=[]
-
-    while not pila.isEmpty():
-        pilaSolucion.append(pila.pop())
-
-    return pilaSolucion
-
-
-def algoritmoBusqueda(problema, estrategia, profundidaMaxima):
-
-    dictPoda={}
-
-    frontera=Frontera()
-    estadoInicial=problema.getEstadoInicial()
-    nodoInicial=generarNodoArbol(None, estadoInicial, 0, estrategia, '0.0 0 0.0')
-    frontera.anadirNodo(nodoInicial)
-    solucion=None
-
-    while ((solucion == None) and (not (frontera.estaVacia()))):
-        nodoActual=frontera.sacar()
-        estadoNodoActual=nodoActual.getEstado()
-        #print("nodo actual", nodoActual.getAccion(), estadoNodoActual.getListaNodos())
-        if problema.esObjetivo(estadoNodoActual):
-            solucion=True
-        else:
-            sucesoresEstado=problema.getEspacioEstados().sucesores(estadoNodoActual)
-            if sucesoresEstado != None:
-                #print("s", sucesoresEstado)
-                listaNodos=expandirNodos(
-                    sucesoresEstado, nodoActual, profundidaMaxima, estrategia)
-                nodosPoda, dictPoda = algoritmoPoda(
-                    dictPoda, listaNodos, estrategia)
-                frontera.anadirListaNodos(nodosPoda)
-
-    if (solucion == None):
-        return None, None
-    else:
-        return formatoSolucion(nodoActual), nodoActual
-
-
-def busquedaSolucion(problema, estrategia, profundidadMaxima):
-
-    #prof_Actual = 0
-    solucion = None
-
-    while ((solucion == None)):
-        #global nodosCreados
-        #nodosCreados = 0
-        solucion, ultimoNodoVisitado = algoritmoBusqueda(problema, estrategia, profundidadMaxima)
-        #prof_Actual = prof_Actual + 1
-
-    return solucion, ultimoNodoVisitado
+    while nodo.getPadre() is not None:
+        solucion.append(nodo)
+        nodo = nodo.getPadre()
+    solucion.append(nodo)
+    return solucion
 
 
 if __name__ == "__main__":
@@ -137,9 +102,7 @@ if __name__ == "__main__":
 
     profundidad = 0
     profundidadMaxima = 68
-    Estrategia = "anchura"
-    # nodoInicial = "123"
-    # listaNodosInicial = ['12', '10', '122']
+    Estrategia = "costo"
     nodoInicial = "54"
     listaNodosInicial = ['186', '699', '1277']
     problema = Problema(nodoInicial, listaNodosInicial)
@@ -148,15 +111,16 @@ if __name__ == "__main__":
         problema, Estrategia, profundidadMaxima)
 
     if (solucion is not None):
-        # Se escribe la solucion en un archivo .txt
-        print("Ultimo nodo visitado:", ultimoNodoVisitado.getIDNodo())
-        print("Estrategia usada:", Estrategia)
-        print("Problema:",  problema.getEstadoInicial().getNodo(), problema.getEstadoInicial().getListaNodos())
-        for nodo in solucion:
+        print("SOLUCION ENCONTRADA")
+        print("- Problema:",  problema.getEstadoInicial().getNodo(),
+              problema.getEstadoInicial().getListaNodos())
+        print("- Estrategia usada:", Estrategia)
+        print("- Ultimo nodo visitado:", ultimoNodoVisitado.getIDNodo())
+        for nodo in reversed(solucion):
             listaNodosSolucion.append(nodo.getIDNodo())
         print(listaNodosSolucion)
-        print("Profundidad", ultimoNodoVisitado.getProfundidad())
-        print("Coste", round(ultimoNodoVisitado.getCoste(),2))
+        print("- PROFUNDIDAD:", ultimoNodoVisitado.getProfundidad())
+        print("- COSTE:", round(ultimoNodoVisitado.getCoste(), 2))
         print("FIN DEL PROGRAMA")
     else:
         print("NO HAY SOLUCIÓN")
